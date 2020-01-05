e616dd6a67af14748176f31c9674af9fbc768717
Fayaz Sayed <fsayed646@gmail.com>
2020-01-05 11:20:41 +0530

    postbuild

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 require("dotenv").config();
+const path = require("path");
 const express = require("express");
 const app = express();
+app.use(express.static(path.join(__dirname, "client/build")));
 const port = process.env.PORT || 5000;
 
 require("./routes")(app);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 app.listen(port, () =>
   console.info("Server started. Listening to port: " + port)
 );
